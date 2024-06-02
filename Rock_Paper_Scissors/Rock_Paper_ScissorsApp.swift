e786789f80867249edb518b39efe4468280271c9
grant-dot-dev <12433404+grant-dot-dev@users.noreply.github.com>
2024-06-02 16:55:41 +0100

    Initial Commit

---
+++ Rock_Paper_Scissors/Rock_Paper_ScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_Paper_ScissorsApp.swift
+//  Rock_Paper_Scissors
+//
+//  Created by Grant  Riordan on 02/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Rock_Paper_ScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
